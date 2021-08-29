d0f49a6e4ee83294249fa60d959dd5f5e0c2a304
John Patrick Valera <johnpatrickyusoresvalera@gmail.com>
2021-08-29 13:01:45 +0800

    Added solution for Linked List, problem 4

--- Linked List/Problems/4.py
+++ Linked List/Problems/4.py
@@ -5,12 +5,39 @@ from Modules.SLL import SLL
 
 
 def mergeSorted(a, b) -> SLL:
-    result = SLL()
-    temp = None
-    a_ = a.getHeadNode()
-    b_ = b.getHeadNode()
 
-    return result
+    def merge(arrA, arrB):
+        result = [arrA + arrB]
+        for i, data in enumerate(result):
+            for j in range(i, len(result)):
+                current = result[j]
+                if data > current:
+                    temp = data
+                    result[i] = current
+                    result[j] = temp
+        return result
+
+    def split(arr):
+        if len(arr) == 1:
+            return arr
+        else:
+            import math
+            midP = math.ceil(len(arr)/2)
+            arrA = [i for i in range(midP)]
+            arrB = [i for i in range(midP, len(arr))]
+            return split(merge(arrA, arrB))
+
+    a_, b_, a_data, b_data = a.getHeadNode(), b.getHeadNode(), [], []
+    while a_.getNext() != None:
+        a_data.append(a_.getData())
+        a_ = a_.getNext()
+    while b_.getNext() != None:
+        b_data.append(b_.getData())
+        b_ = b_.getNext()
+    result, retResult = split(a_data + b_data), SLL()
+    for each in result:
+        retResult.insert(each)
+    return retResult
 
 
 if __name__ == "__main__":
@@ -30,7 +57,7 @@ if __name__ == "__main__":
         test_data_b.insert(i)
 
     test_data_a.printList()
-    test_data_b.printList()\
+    test_data_b.printList()
 
     merged = mergeSorted(test_data_a, test_data_b)
     merged.printList()
